7fe3c47107350b373a9bc806802ddcb5544f4e0b
Phils Garcia <eivanphils@gmail.com>
2022-05-12 13:00:30 -0400

    Agregado ListTile Widget

--- lib/screens/listview1_screen.dart
+++ lib/screens/listview1_screen.dart
@@ -1,23 +1,26 @@
 import 'package:flutter/material.dart';
 
 class ListViewScreen extends StatelessWidget {
-   
+  final options = const ['Megaman', 'Pokemon', 'Digimon', 'Dragon Ball Z'];
+
   const ListViewScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('ListView1'),
-      ),
-      body: ListView(
-        children: const [
-          Text('List 1'),
-          Text('List 1'),
-          Text('List 1'),
-          Text('List 1'),
-        ],
-      )
-    );
+        appBar: AppBar(
+          title: const Text('ListView1'),
+        ),
+        body: ListView(
+          children: [
+            ...options.map(
+              (option) => ListTile(
+                leading: const Icon(Icons.access_time_filled),
+                title: Text(option),
+                trailing: const Icon(Icons.ac_unit_rounded),
+              )
+            ).toList()
+          ],
+        ));
   }
 }
