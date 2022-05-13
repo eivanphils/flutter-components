0b08c6e1625a9fc056c8d2fa8751cf0d2e1f6b80
Phils Garcia <eivanphils@gmail.com>
2022-05-13 13:05:07 -0400

    Modificado estilos de la pantalla

--- lib/screens/listview2_screen.dart
+++ lib/screens/listview2_screen.dart
@@ -10,14 +10,17 @@ class ListView2Screen extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           title: const Text('ListView tipo 2'),
+          elevation: 0,
+          backgroundColor: Colors.indigo,
         ),
         body: ListView.separated(
             itemCount: options.length,
             itemBuilder: (context, index) {
               return ListTile(
-                leading: const Icon(Icons.access_time_filled),
+                leading: const Icon(Icons.access_time_filled, color: Colors.indigo),
                 title: Text(options[index]),
-                trailing: const Icon(Icons.ac_unit_rounded),
+                trailing: const Icon(Icons.ac_unit_rounded, color: Colors.red),
+                onTap: () => print('tab $index'),
               );
             },
             separatorBuilder: (_, __) => const Divider()));
