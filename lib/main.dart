f3d446157bb21788cc103cfbf1ca98edaab8758e
Phils Garcia <eivanphils@gmail.com>
2022-05-13 12:47:57 -0400

    Agregado ListView.separated widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_components/screens/listview1_screen.dart';
+
+import 'package:flutter_components/screens/listview2_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: ListViewScreen()
+      home: ListView2Screen()
     );
   }
 }
---
+++ lib/screens/listview2_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ListView2Screen extends StatelessWidget {
+  final options = const ['Megaman', 'Pokemon', 'Digimon', 'Dragon Ball Z'];
+
+  const ListView2Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('ListView tipo 2'),
+        ),
+        body: ListView.separated(
+            itemCount: options.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                leading: const Icon(Icons.access_time_filled),
+                title: Text(options[index]),
+                trailing: const Icon(Icons.ac_unit_rounded),
+              );
+            },
+            separatorBuilder: (_, __) => const Divider()));
+  }
+}
