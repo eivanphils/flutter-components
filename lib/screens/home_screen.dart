8f9af480b7d7335541982fbd395f18b88421fd6f
Phils Garcia <eivanphils@gmail.com>
2022-05-13 14:17:06 -0400

    Agregado método para usar la navegacion a una pantalla en especifico

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,26 +1,35 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_components/screens/screens.dart';
+
 class HomeScreen extends StatelessWidget {
-   
   const HomeScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Componentes de Flutter'),
-      ),
-      body: ListView.separated(
-        itemBuilder: (context, index) => ListTile(
-          leading: const Icon(Icons.analytics),
-          title: const Text('Mi componente'),
-          onTap: () {
-            print('click $index');
-          },
+        appBar: AppBar(
+          title: const Text('Componentes de Flutter'),
         ),
-        separatorBuilder: (_, __) => const Divider(),
-        itemCount: 40
-      )
-    );
+        body: ListView.separated(
+            itemBuilder: (context, index) => ListTile(
+                  leading: const Icon(Icons.analytics),
+                  title: const Text('Mi componente'),
+                  onTap: () {
+                    // Armando la ruta completa
+                    // final route = MaterialPageRoute(
+                    //   builder: (context) => const ListViewScreen(),
+                    // );
+
+                    // Navigator.push(context, route);
+
+                    Navigator.pushNamed(context, 'listview');
+
+                    // Borrar la ruta anterior del historial
+                    // Navigator.pushReplacementNamed(context, 'listview');
+                  },
+                ),
+            separatorBuilder: (_, __) => const Divider(),
+            itemCount: 40));
   }
 }
