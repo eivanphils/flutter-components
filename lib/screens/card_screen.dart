2ad87d04d137b85bd5488e531de8f307dfd1b9be
Phils Garcia <eivanphils@gmail.com>
2022-05-15 13:33:38 -0400

    Agregado parametros opcionales en el card widget

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -20,12 +20,25 @@ class CardScreen extends StatelessWidget {
             subtitle: 'Est aute mollit aliquip pariatur adipisicing in. Est aute mollit aliquip pariatur adipisicing in.',
             icon: Icons.photo_album
           ),
+          SizedBox(height: 10),
           CustomCard(
             title: 'Titulo dentro del card 2',
             subtitle: 'Est aute mollit aliquip pariatur adipisicing in. Est aute mollit aliquip pariatur adipisicing in. Est aute mollit aliquip pariatur adipisicing in. 2',
             icon: Icons.lock_clock
           ),
-          CustomCardType2()
+          SizedBox(height: 10),
+          CustomCardType2(),
+          SizedBox(height: 10),
+          CustomCardType2(
+            image: 'https://d1fytg1nnnd8c0.cloudfront.net/wp-content/uploads/2022/01/WZP-S1-ANNOUNCEMENT-009.jpg',
+            title: 'Warzone - Vanguard',
+          ),
+          SizedBox(height: 10),
+          CustomCardType2(
+            image: 'https://i2.wp.com/www.pcmrace.com/wp-content/uploads/2019/05/APlagueTaleInnocence_x64-2019-05-05-05-28-13-822.jpg?ssl=1',
+            title: 'A Plague Tale',
+          ),
+          SizedBox(height: 10),
         ],
       )
     );
--- lib/widgets/custom_card_type_2.dart
+++ lib/widgets/custom_card_type_2.dart
@@ -1,22 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_components/theme/app_theme.dart';
 
 class CustomCardType2 extends StatelessWidget {
-  const CustomCardType2({Key? key}) : super(key: key);
+  final String image;
+  final String? title;
+
+  const CustomCardType2({
+    Key? key,
+    this.image = 'https://images3.alphacoders.com/997/997848.jpg',
+    this.title,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      elevation: 10,
+      clipBehavior: Clip.antiAlias,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(18)
+      ),
+      elevation: 20,
+      shadowColor: AppTheme.primaryColor.withOpacity(0.8),
       child: Column(
-        children: const [
+        children: [
           // Una manera de agregar la imagen
           // Image(
           //   image: NetworkImage('https://images3.alphacoders.com/997/997848.jpg')
           // )
           FadeInImage(
-            placeholder: AssetImage('assets/jar-loading.gif'),
-            image: NetworkImage('https://images3.alphacoders.com/997/997848.jpg')
-          )
+            placeholder: const AssetImage('assets/jar-loading.gif'),
+            image: NetworkImage(image),
+            width: double.infinity,
+            height: 230,
+            fit: BoxFit.cover,
+            fadeInDuration: const Duration(milliseconds: 300),
+          ),
+          if(title != null)
+            Container(
+              alignment: AlignmentDirectional.centerEnd,
+              padding: const EdgeInsets.only(right: 20, top: 10, bottom: 10),
+              child: Text(title!),
+            )
         ],
       ),
     );
