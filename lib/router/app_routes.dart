dd7fd8694ce556083995c161f154966d90aceab8
Phils Garcia <eivanphils@gmail.com>
2022-05-16 16:13:23 -0400

    Eliminado el home del listado de wigdets

--- lib/router/app_routes.dart
+++ lib/router/app_routes.dart
@@ -7,7 +7,8 @@ class AppRoutes {
   static const initialRoute = 'home';
 
   static final menuOptions = <MenuOption>[
-    MenuOption(route: 'home', name: 'Home Screen', screen: const HomeScreen(), icon: Icons.home),
+    // Ocultado homeScreen para que no aparezca en el home
+    // MenuOption(route: 'home', name: 'Home Screen', screen: const HomeScreen(), icon: Icons.home),
     MenuOption(route: 'listview', name: 'Listview tipo 1', screen: const ListViewScreen(), icon: Icons.list),
     MenuOption(route: 'listview2', name: 'Listview tipo 2', screen: const ListView2Screen(), icon: Icons.list_alt),
     MenuOption(route: 'card', name: 'Tarjetas - Card', screen: const CardScreen(), icon: Icons.credit_card),
@@ -26,7 +27,7 @@ class AppRoutes {
 
   static Map<String, Widget Function(BuildContext)> getAppRoutes() {
     Map<String, Widget Function(BuildContext)> appRoutes = {};
-
+    appRoutes.addAll({ 'home': (BuildContext context) => const HomeScreen()});
     for (final option in menuOptions) {
       appRoutes.addAll({
         option.route: (BuildContext context) => option.screen
