061e128640480111fbf9d935ea096b227734d6c5
Phils Garcia <eivanphils@gmail.com>
2022-05-17 15:44:05 -0400

    Agregado estilos y propiedades al TextFormField Widget

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -14,21 +14,40 @@ class InputsScreen extends StatelessWidget {
             padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
             child: Column(
               children: [
-                TextField(
-                  autofocus: true,
-                  onChanged: (value) {
-                    print(value);
-                  },
-                ),
                 TextFormField(
-                  autofocus: true,
-                  initialValue: 'Mi nombre',
+                  autofocus: false,
+                  initialValue: '',
                   maxLength: 15,
                   autocorrect: false,
                   textCapitalization: TextCapitalization.words,
                   onChanged: (value) {
                     print(value);
                   },
+                  validator: (value) {
+                    if (value == null) {
+                      return 'El campo es obligatorio';
+                    }
+
+                    return value.length < 3 ? 'Minimo 3 caracteres' : null;
+                  },
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  decoration: const InputDecoration(
+                    hintText: 'Nombre del usuario',
+                    labelText: 'Nombre',
+                    floatingLabelBehavior: FloatingLabelBehavior.auto,
+                    helperText: 'Solo texto',
+                    prefixIcon: Icon(Icons.abc_sharp),
+                    suffixIcon: Icon(Icons.input_sharp),
+                    icon: Icon(Icons.credit_card),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20))
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.green
+                      )
+                    )
+                  ),
                 )
               ],
             ),
