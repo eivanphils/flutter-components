e74e326057025ba5b66cda8701e6265c538669ca
Phils Garcia <eivanphils@gmail.com>
2022-05-24 13:50:18 -0400

    Agregado DropdownButtonFormField Widget con valores de un listado

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -18,6 +18,8 @@ class InputsScreen extends StatelessWidget {
       'role': 'Admin'
     };
 
+    const List<String> roles = ['Admin', 'SuperUser', 'Guest', 'User'];
+
     return Scaffold(
         appBar: AppBar(
           title: const Text('Inputs - Form'),
@@ -30,49 +32,60 @@ class InputsScreen extends StatelessWidget {
               child: Column(
                 children: [
                   CustomInputField(
-                    labelText: 'Nombre',
-                    hintText: 'Nombre de usuario',
-                    formValues: formValues,
-                    formProperty: 'name'
-                  ),
+                      labelText: 'Nombre',
+                      hintText: 'Nombre de usuario',
+                      formValues: formValues,
+                      formProperty: 'name'),
                   const SizedBox(height: 20),
                   CustomInputField(
-                    labelText: 'Tarjeta',
-                    hintText: 'Nro de tarjeta',
-                    helperText: 'Introduce el valor de tu tarjeta',
-                    icon: Icons.card_giftcard,
-                    suffixIcon: Icons.check,
-                    keyboardType: TextInputType.number,
-                    formValues: formValues,
-                    formProperty: 'card_number'
-                  ),
+                      labelText: 'Tarjeta',
+                      hintText: 'Nro de tarjeta',
+                      helperText: 'Introduce el valor de tu tarjeta',
+                      icon: Icons.card_giftcard,
+                      suffixIcon: Icons.check,
+                      keyboardType: TextInputType.number,
+                      formValues: formValues,
+                      formProperty: 'card_number'),
                   const SizedBox(height: 20),
                   CustomInputField(
-                    labelText: 'Correo',
-                    hintText: 'Correo electronico',
-                    suffixIcon: Icons.mail,
-                    keyboardType: TextInputType.emailAddress,
-                    formValues: formValues,
-                    formProperty: 'email'
+                      labelText: 'Correo',
+                      hintText: 'Correo electronico',
+                      suffixIcon: Icons.mail,
+                      keyboardType: TextInputType.emailAddress,
+                      formValues: formValues,
+                      formProperty: 'email'),
+                  const SizedBox(height: 20),
+                  CustomInputField(
+                      labelText: 'Password',
+                      hintText: 'Password del usuario',
+                      obscureText: true,
+                      formValues: formValues,
+                      formProperty: 'password'),
+                  const SizedBox(height: 20),
+                  CustomInputField(
+                      labelText: 'Url',
+                      hintText: 'pagina web',
+                      obscureText: false,
+                      keyboardType: TextInputType.url,
+                      formValues: formValues,
+                      formProperty: 'website'),
+                  const SizedBox(height: 30),
+                  DropdownButtonFormField<String>(
+                      //* agregado un valor fijo
+                      // items: const [
+                      //   DropdownMenuItem(value: 'admin', child: Text('admin')),
+                      // ],
 
-                  ),
-                  const SizedBox(height: 20),
-                  CustomInputField(
-                    labelText: 'Password',
-                    hintText: 'Password del usuario',
-                    obscureText: true,
-                    formValues: formValues,
-                    formProperty: 'password'
-                  ),
-                  const SizedBox(height: 20),
-                  CustomInputField(
-                    labelText: 'Url',
-                    hintText: 'pagina web',
-                    obscureText: true,
-                    keyboardType: TextInputType.url,
-                    formValues: formValues,
-                    formProperty: 'website'
-                  ),
+                      //* agregando un valor de un listado
+                      items:
+                          roles.map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                            value: value, child: Text(value));
+                      }).toList(),
+                      onChanged: (value) {
+                        print(value);
+                        formValues['role'] = value ?? 'Admin';
+                      }),
                   const SizedBox(height: 30),
                   ElevatedButton(
                     child: const SizedBox(
@@ -85,7 +98,6 @@ class InputsScreen extends StatelessWidget {
                               fontSize: 17, fontWeight: FontWeight.bold),
                         ))),
                     onPressed: () {
-
                       //* Forma de ocultar el keyboard al presionar el boton del formulario
                       FocusManager.instance.primaryFocus?.unfocus();
 
