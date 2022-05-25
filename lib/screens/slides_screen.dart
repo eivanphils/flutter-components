eab0ea88a19d0b08d045bdbab062b56f7b3ba931
Phils Garcia <eivanphils@gmail.com>
2022-05-25 19:53:25 -0400

    Agregado Checkbox, CheckboxListTile, Switch, SwitchListTile & AboutListTile Widget

--- lib/screens/slides_screen.dart
+++ lib/screens/slides_screen.dart
@@ -11,6 +11,7 @@ class SlidesScreen extends StatefulWidget {
 
 class _SlidesScreenState extends State<SlidesScreen> {
   double _sliderValue = 100;
+  bool _sliderEnabled = true;
 
   @override
   Widget build(BuildContext context) {
@@ -18,32 +19,81 @@ class _SlidesScreenState extends State<SlidesScreen> {
       appBar: AppBar(
         title: const Text('Slides & Checks'),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Slider.adaptive(
-              min: 50,
-              max: 500,
-              value: _sliderValue,
-              thumbColor: AppTheme.primary,
-              activeColor: AppTheme.primary,
-              inactiveColor: AppTheme.primary.withOpacity(0.3),
-              onChanged: (double value) {
+      body: Column(
+        children: [
+          Slider.adaptive(
+            min: 50,
+            max: 1000,
+            value: _sliderValue,
+            thumbColor: AppTheme.primary,
+            activeColor: AppTheme.primary,
+            inactiveColor: AppTheme.primary.withOpacity(0.3),
+            onChanged: _sliderEnabled
+            ? (double value) {
                 print(value);
                 _sliderValue = value;
                 setState(() {});
-              },
+              }
+            : null,
+          ),
+          Checkbox(
+            value: _sliderEnabled,
+            onChanged: (value) {
+              _sliderEnabled = value ?? true;
+              setState(() {});
+            }
+          ),
+          CheckboxListTile(
+            title: Text(
+              _sliderEnabled ? 'Desactivar CheckboxListTile' : 'Activar CheckboxListTile',
+              style: const TextStyle(color: Colors.white)
             ),
-      
-            Image(
-              image: NetworkImage('https://www.pngmart.com/files/3/Uchiha-Sasuke-PNG-Picture.png'),
-              fit: BoxFit.contain,
-              width: _sliderValue,
+            tileColor: AppTheme.secondary.withOpacity(0.9),
+            checkColor: AppTheme.primary,
+            activeColor: Colors.white,
+            controlAffinity: ListTileControlAffinity.leading,
+            secondary: const Icon(Icons.person, color: Colors.white),
+            value: _sliderEnabled,
+            onChanged: (value) {
+              _sliderEnabled = value ?? true;
+              setState(() {});
+            }
+          ),
+
+          Switch(
+            value: _sliderEnabled,
+            onChanged: (value) {
+              _sliderEnabled = value;
+              setState(() {});
+            }
+          ),
+
+          SwitchListTile.adaptive(
+            title: Text(
+              _sliderEnabled ? 'Desactivar CheckboxListTile' : 'Activar CheckboxListTile'
             ),
-      
-            const SizedBox(height: 50)
-          ],
-        ),
+            controlAffinity: ListTileControlAffinity.platform,
+            secondary: const Icon(Icons.person),
+            value: _sliderEnabled,
+            onChanged: (value) {
+              _sliderEnabled = value;
+              setState(() {});
+            }
+          ),
+
+          const AboutListTile(),
+
+          Expanded(
+            child: SingleChildScrollView(
+              child: Image(
+                image: const NetworkImage(
+                    'https://c.wallhere.com/photos/83/3b/Star_Wars_Rogue_One_A_Star_Wars_Story_Darth_Vader_Jyn_Erso_Felicity_Jones_portrait_display-6468.png!d'),
+                fit: BoxFit.contain,
+                height: _sliderValue,
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
