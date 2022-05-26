e0de76ae27fbc6eed5ba8eb324cc591f4894798d
Phils Garcia <eivanphils@gmail.com>
2022-05-26 18:51:30 -0400

    Agregado scroll infinito

--- lib/screens/list_view_builder_screen.dart
+++ lib/screens/list_view_builder_screen.dart
@@ -1,7 +1,35 @@
 import 'package:flutter/material.dart';
 
-class ListViewBuilderScreen extends StatelessWidget {
+class ListViewBuilderScreen extends StatefulWidget {
   const ListViewBuilderScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ListViewBuilderScreen> createState() => _ListViewBuilderScreenState();
+}
+
+class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
+  final List<int> imagesId = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  final ScrollController scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    scrollController.addListener(() {
+      // print('${scrollController.position.pixels}, ${scrollController.position.maxScrollExtent}');
+      if (scrollController.position.pixels + 500 >=
+          scrollController.position.maxScrollExtent) {
+        loadPhotos();
+      }
+    });
+  }
+
+  void loadPhotos() {
+    final int lastId = imagesId.last;
+    imagesId.addAll([1, 2, 3, 4, 5].map((n) => lastId + n));
+
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,11 +40,13 @@ class ListViewBuilderScreen extends StatelessWidget {
         removeBottom: true,
         context: context,
         child: ListView.builder(
-          itemCount: 10,
+          controller: scrollController,
+          itemCount: imagesId.length,
           itemBuilder: (BuildContext context, int index) {
             return FadeInImage(
               placeholder: const AssetImage('assets/jar-loading.gif'),
-              image: NetworkImage('https://picsum.photos/500/300?image=${index + 1}'),
+              image: NetworkImage(
+                  'https://picsum.photos/500/300?image=${imagesId[index]}'),
               width: double.infinity,
               height: 300,
               fit: BoxFit.cover,
